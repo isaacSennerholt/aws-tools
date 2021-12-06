83ed6b5c8e163206007471033c1a16dd727ed76f
Isaac Sennerholt <isse@mbpsomtnnerholt.mynet>
2021-12-06 06:13:07 +0100

    Add unique method to dynamodb yup model

--- dynamo-db-yup-model/lib/index.js
+++ dynamo-db-yup-model/lib/index.js
@@ -15,60 +15,78 @@ function _reduceShapeToMatchPayload(originShape = {}, payload = {}) {
   }, {})
 }
 
-function define({ tableName, shape, region, endpoint }) {
+function _addMethodUnique(yup, dynamoDb) {
+  yup.addMethod(yup.mixed, 'unique', function(indexName = '', message = '') {
+    return this.test('isUnique', message || 'The ${path} already exists', async function (value) {
+      const { path } = this;
+      const { Items } = await dynamoDb.queryByAttributes(indexName, { [path]: value })
+      return !Items.length
+    })
+  })
+}
+
+function init({ tableName, region, endpoint }) {
   const dynamoDb = dynamoDbSdk.configure({ tableName, region, endpoint })
-  const shapeWithDefaults = { ...shape, ...defaultShape }
+  
+  _addMethodUnique(yup, dynamoDb)
+
   return {
-    getById: async id => {
-      const { Item } = await dynamoDb.getById(id)
-      return Item || null
-    },
-    batchGetByAttributes: async attributes => {
-      const { Responses } = await dynamoDb.batchGetByAttributes(attributes)
-      return Responses[tableName]
-    },
-    queryByAttributes: async (indexName, attributes) => {
-      const { Items } = await dynamoDb.queryByAttributes(indexName, attributes)
-      return Items
-    },
-    scanByAttributes: async (attributes) => {
-      const { Items } = await dynamoDb.scanByAttributes(attributes)
-      return Items
-    },
-    create: async payload => {
-      const schema = yup.object(shapeWithDefaults).noUnknown(true)
-      const payloadWithDefaults = {
-        ...payload,
-        id: uuidv4(),
-        createdAt: new Date().toISOString(),
-        updatedAt: new Date().toISOString(),
+    yup,
+    createModel: ({ shape }) => {
+      const shapeWithDefaults = { ...shape, ...defaultShape }
+      return {
+        getById: async id => {
+          const { Item } = await dynamoDb.getById(id)
+          return Item || null
+        },
+        batchGetByAttributes: async attributes => {
+          const { Responses } = await dynamoDb.batchGetByAttributes(attributes)
+          return Responses[tableName]
+        },
+        queryByAttributes: async (indexName, attributes) => {
+          const { Items } = await dynamoDb.queryByAttributes(indexName, attributes)
+          return Items
+        },
+        scanByAttributes: async (attributes) => {
+          const { Items } = await dynamoDb.scanByAttributes(attributes)
+          return Items
+        },
+        create: async payload => {
+          const schema = yup.object(shapeWithDefaults).noUnknown(true)
+          const payloadWithDefaults = {
+            ...payload,
+            id: uuidv4(),
+            createdAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString(),
+          }
+    
+          await schema.validate(payloadWithDefaults, { strict: true })
+          return dynamoDb.create(payloadWithDefaults)
+        },
+        updateById: async (id, payload) => {
+          const payloadWithDefaults = { ...payload, updatedAt: new Date().toISOString() }
+          const shapeFromPayload = _reduceShapeToMatchPayload(
+            shapeWithDefaults,
+            payloadWithDefaults
+          )
+    
+          const shapeToUse = Object.keys(shapeFromPayload).length
+            ? shapeFromPayload
+            : shapeWithDefaults
+    
+          const schema = yup.object(shapeToUse).noUnknown(true)
+    
+          await schema.validate(payloadWithDefaults, { strict: true })
+          const { Attributes } = await dynamoDb.updateById(id, payloadWithDefaults)
+          return Attributes || null
+        },
+        deleteById: async id => {
+          const { Attributes } = await dynamoDb.deleteById(id)
+          return Attributes || null
+        },
       }
-
-      await schema.validate(payloadWithDefaults, { strict: true })
-      return dynamoDb.create(payloadWithDefaults)
-    },
-    updateById: async (id, payload) => {
-      const payloadWithDefaults = { ...payload, updatedAt: new Date().toISOString() }
-      const shapeFromPayload = _reduceShapeToMatchPayload(
-        shapeWithDefaults,
-        payloadWithDefaults
-      )
-
-      const shapeToUse = Object.keys(shapeFromPayload).length
-        ? shapeFromPayload
-        : shapeWithDefaults
-
-      const schema = yup.object(shapeToUse).noUnknown(true)
-
-      await schema.validate(payloadWithDefaults, { strict: true })
-      const { Attributes } = await dynamoDb.updateById(id, payloadWithDefaults)
-      return Attributes || null
-    },
-    deleteById: async id => {
-      const { Attributes } = await dynamoDb.deleteById(id)
-      return Attributes || null
-    },
+    }
   }
 }
 
-module.exports = { define }
+module.exports = { init }
